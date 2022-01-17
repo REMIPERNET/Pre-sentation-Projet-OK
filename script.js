f5b91227a8cc027a859bb3ae53cff68b1d9a23d9
REMIPERNET <rpernet@orange.fr>
2022-01-17 09:55:09 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const spaceHolder = document.querySelectorAll('.space-holder');
+const horizontal = document.querySelectorAll('.horizontal');
+const sticky = document.querySelectorAll('.sticky');
+
+for (let i = 0; i < spaceHolder.length; i++) {
+  spaceHolder[i].style.height = `${calcDynamicHeight(horizontal[i])}px`;
+
+function calcDynamicHeight(ref) {
+  const vw = window.innerWidth;
+  const vh = window.innerHeight;
+  const objectWidth = ref.scrollWidth;
+  return objectWidth - vw + vh + 150; // 150 est le padding (en pixels) desiré sur la droite de la .cards container.
+}
+
+window.addEventListener('scroll', () => {
+  horizontal[i].style.transform = `translateX(-${sticky[i].offsetTop}px)`;
+});
+
+window.addEventListener('resize', () => {
+  spaceHolder[i].style.height = `${calcDynamicHeight(horizontal[i])}px`;
+});
+
+}
